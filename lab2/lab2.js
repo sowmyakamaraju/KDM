c4e1fd9222a00f9d7476ad73b38a955fc656e43b
sowmyakamaraju <sk38d@mail.umkc.edu>
2014-02-04 22:37:08 -0600

    lab2 commit
    lab2

---
+++ lab2/lab2.js
@@ -0,0 +1,12 @@
+ function showpics(){
+
+    var pic= $("#box").val();
+    $.getJSON('http://api.flickr.com/services/feeds/photos_public.gne?tags='
+      +pic+"&tagmode=any&format=json&jsoncallback=?",
+      function(data){ $("#images").hide().html(data).fadeIn('fast');
+
+    $.each(data.items, function(i,item) {
+      $("<img/>").attr("src", item.media.m).appendTo("#images");
+    });
+  })
+};
